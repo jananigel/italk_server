e4253ba8d0ca0d497ba53f0bb655718b3b44ffb6
kyle <kyle.chang@ackuretta.com>
2026-01-21 21:11:12 +0800

    feat: add the root route to get the server information

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.json({
+    status: 'italk backend running',
+    api: '/api'
+  });
+});
+
 app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
