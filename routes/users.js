f970fe49e3f1ec5cf5123f898ec126f39dce80b9
kyle <kyle.chang@ackuretta.com>
2026-01-21 21:15:55 +0800

    feat: update the api of the users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const chatRoom = require('../services/chat-room');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', (req, res) => {
+  res.json({
+    count: chatRoom.count(),
+    users: chatRoom.listUsers()
+  });
 });
 
 module.exports = router;
